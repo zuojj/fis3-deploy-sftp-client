af2f1fe7f2db9fe45f110686e734d7128eb88179
zuojj <cuew1987@163.com>
2017-03-01 19:53:26 +0800

    :tada:

--- README.md
+++ README.md
@@ -1,20 +1,47 @@
 # fis3-deploy-sftp-client
 
 [![Latest NPM release][npm-badge]][npm-badge-url]
-[![TravisCI Build Status][travis-badge]][travis-badge-url]
-[![Test Coverage][coveralls-badge]][coveralls-badge-url]
-[![Code Climate][codeclimate-badge]][codeclimate-badge-url]
 
 [npm-badge]: https://img.shields.io/npm/v/fis3-deploy-sftp-client.svg
 [npm-badge-url]: https://www.npmjs.com/package/fis3-deploy-sftp-client
-[travis-badge]: https://img.shields.io/travis/zuojj/fis3-deploy-sftp-client/master.svg?label=TravisCI
-[travis-badge-url]: https://travis-ci.org/zuojj/fis3-deploy-sftp-client
-[coveralls-badge]: https://img.shields.io/coveralls/zuojj/fis3-deploy-sftp-client/master.svg
-[coveralls-badge-url]: https://coveralls.io/github/zuojj/fis3-deploy-sftp-client
-[codeclimate-badge]: https://img.shields.io/codeclimate/github/zuojj/fis3-deploy-sftp-client.svg
-[codeclimate-badge-url]: https://codeclimate.com/github/zuojj/fis3-deploy-sftp-client
 
 
 
 It is a deploy files tool from sftp client based on ssh2
 
+
+## Defaults
+
+```javascript
+{
+    host: "",
+    port: 22,
+    username: '',
+    password: '',
+    /* filter by fis release path  */
+    from: '',
+    /* remote linux path */
+    to: '',
+    cache: true
+}
+```
+
+## Install
+
+```shell
+npm install [-g] fis3-deploy-sftp-client
+```
+
+## use
+
+```javascript
+fis.match('*', {
+    deploy: fis.plugin('sftp-client', {
+        from: ['/resource/static', '/view'],
+        to: ['/test2/a', '/test2/b'],
+        host: '10.134.99.182',
+        username: 'root',
+        password: 'ZJ$011023'
+    })
+})
+```
--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-
 /**
  * @author zuojj
  * @description fis3-deploy-sftp-client
  */
 
-
 const path = require('path');
-const fs   = require('fs');
-const Client = require('ssh2');
+const fs = require('fs');
+const parents = require('parents');
 const async = require('async');
 const assign = require('object-assign');
 const md5File = require('md5-file');
+const Client = require('ssh2');
 
-
-
-module.exports = function (options, modifiedFiles, total, callback) {
+module.exports = function(options, modifiedFiles, total, callback) {
     new SftpClient(options, modifiedFiles, total, callback);
 }
 
@@ -33,7 +30,6 @@ function SftpClient(options, modifiedFiles, total, callback) {
         password: '',
         from: '',
         to: '',
-        excludePattern: null,
         cache: true
     }, options);
 
@@ -48,6 +44,8 @@ function SftpClient(options, modifiedFiles, total, callback) {
     this.options = options;
     this.modifiedFiles = modifiedFiles;
     this.cache = {};
+
+    this.uploadedCache();
     this.uploadFiles();
 }
 
@@ -57,70 +55,147 @@ function SftpClient(options, modifiedFiles, total, callback) {
  * @return {[type]}     [description]
  */
 function print(str) {
-    console.log(str);
+    console.log(' ' + str);
 }
+
+/**
+ * [now description]
+ * @param  {[type]} withoutMilliseconds [description]
+ * @return {[type]}                     [description]
+ */
+function now(withoutMilliseconds) {
+    var d = new Date(),
+        str;
+
+    str = [
+        d.getHours(),
+        d.getMinutes(),
+        d.getSeconds()
+    ].join(':').replace(/\b\d\b/g, '0$&');
+
+    if (!withoutMilliseconds) {
+        str += '.' + ('00' + d.getMilliseconds()).substr(-3);
+    }
+    return str;
+}
+
+
 
 /**
  * [uploadFiles description]
  * @return {[type]} [description]
  */
-SftpClient.prototype.uploadFiles = function () {
+SftpClient.prototype.uploadFiles = function() {
     var self = this,
         opts = self.options,
+        from = opts.from,
+        from = from && Array.isArray(from) ? from : [],
+        to   = opts.to,
+        to   = to && Array.isArray(to) ? to : [],
         cache = self.cache,
-        files = self.modifiedFiles;
+        files = self.modifiedFiles,
+        filesLen = files.length;
 
-    self.getClient(function (sftp) {
+    self.getClient(function(sftp) {
         var fileCount = 0;
 
-        async.eachSeries(files, function (file, done) {
-            let releasePath = file.getHashRelease(),
-                content  = file.getContent(),
+        // cache direcotry made 
+        cache.mkdir = {};
+
+        async.eachSeries(files, function(file, done) {
+            let releasepath = file.getHashRelease(),
+                content = file.getContent(),
                 realpath = file.realpath,
-                subpath  = file.subpath,
-                topath   = path.join(opts.to, release);
+                subpath = file.subpath,
+                topath,
+                mkdirArr;
 
-            async.whilst(function () {
-                return true;
-            }, function (next) {
-                sftp.mkdir(path.dirname(topath), { mode: '0755' }, function () {
-                    process.stdout.write(" " + '\u2714'.green);
-                    next();
-                });
-            }, function () {
-                let readStream = fs.createReadStream(realpath);
-                let writeStream = sftp.createWriteStream(topath, {
-                    flags: 'w',
-                    encoding: null,
-                    mode: '0666',
-                    autoClose: true
-                });
-
-                let time = '[' + fis.log.now(true) + ']';
-
-                process.stdout.write(
-                    '\n - '.green.bold +
-                    time.grey + ' ' +
-                    subpath.replace(/^\//, '') +
-                    ' >> '.yellow.bold +
-                    topath
-                );
-
-                readStream.pipe(writeStream);
-                writeStream.on('close', function (err) {
-                    if (err) {
-                        throw new Error('sftp', err);
-                    } else {
-                        fileCount ++ ;
-                        process.stdout.write(" " + ('\u2714').green);
-                    }
-                    done();
-                });
+            from.forEach(function(item, index) {
+                if(releasepath.indexOf(item) === 0 && to[index]) {
+                    topath = path.join(to[index], releasepath).replace(/\\/g, '/')
+                }
             });
-        }, function () {
-            print('\nsftp:', (fileCount, fileCount === 1 ? 'file' : 'files', 'uploaded successfully').green);
+
+            if(!topath) {
+                done();
+                return;
+            }
+
+            /* remove root directory */
+            mkdirArr = parents(path.dirname(topath).replace(/^\//, '')) || [];
+            mkdirArr = mkdirArr.map((item) => {
+                item = item.replace(/^\/~/, '~').replace(/\\/g, '/');
+                return /^\//.test(item) ? item : ('/' + item);
+            }).filter((item) => {
+                return !cache.mkdir[item];
+            });
+
+            /* teration make directory */
+            async.whilst(
+                function() {
+                    return mkdirArr.length;
+                },
+                function(next) {
+                    let mkdirpath = mkdirArr.pop();
+
+                    // cache tag
+                    cache.mkdir[mkdirpath] = true;
+
+                    sftp.mkdir(mkdirpath, {
+                        mode: '0755'
+                    }, function(err) {
+                        next();
+                    });
+                },
+                function() {
+                    var fileHash;
+                    let time = '[' + now(true) + ']';
+
+                    let _write = function(str) {
+                        process.stdout.write(['\n - '.green, time.grey + ' ', subpath.replace(/^\//, ''), ' \u00BB '.green,  str].join(''));
+                    }
+                    try {
+                        fileHash = opts.cache && md5File.sync(realpath);
+                        if (fileHash && cache.uploaded[topath] === fileHash) {
+                            _write("haven't changed " + (" skipping").yellow);
+                            done();
+                            return;
+                        }
+                    }catch(e){}
+
+                    let writeStream = sftp.createWriteStream(topath, {
+                        flags: 'w',
+                        encoding: null,
+                        mode: '0666',
+                        autoClose: true
+                    });
+
+                    _write(topath);
+
+                    writeStream.on('close', function(err) {
+                        if (err) {
+                            throw new Error('sftp', err);
+                        } else {
+                            fileCount++;
+                            opts.cache && (cache.uploaded[topath] = fileHash);
+                            process.stdout.write(" " + ('\u2714').green.bold);
+                        }
+                        done();
+                    });
+
+                    writeStream.write(content);
+                    writeStream.end();
+                }
+            );
+        }, function() {
+            print(['\n SFTP:', fileCount, fileCount === 1 ? 'file' : 'files', 'uploaded', 'successfully'].join(' ').green);
 
             cache.finished = true;
+
+            if(opts.cache) {
+                fs.writeFileSync(cache.cacheFilePath, JSON.stringify(cache.uploaded));
+                print("Cache file " + cache.cacheFilePath + " updated");
+            }
 
             if (cache.sftp) {
                 cache.sftp.end();
@@ -137,54 +212,73 @@ SftpClient.prototype.uploadFiles = function () {
  * @param  {Function} callback [description]
  * @return {[type]}            [description]
  */
-SftpClient.prototype.getClient = function (callback) {
+SftpClient.prototype.getClient = function(callback) {
     var self = this,
-        cache = self.cache;
-
-    print('Authenticating with password.');
+        cache = self.cache,
+        opts = self.options;
 
     let client = new Client();
     cache.client = client;
 
-    client.on('ready', function () {
-        client.sftp(function (err, sftp) {
+    client.on('ready', function() {
+        client.sftp(function(err, sftp) {
             if (err) {
                 throw err;
             }
 
-            sftp.on('end', function () {
-                print('SFTP session closed');
+            sftp.on('end', function() {
+                print('SFTP: SESSION CLOSED');
                 sftp = null;
-                if (!finished) {
-                    print('error', new Error('sftp', "SFTP abrupt closure"));
+                if (!cache.finished) {
+                    print('error', new Error("SFTP: ABRUPT CLOSURE"));
                 }
             });
-
+            print("\n CLIENT :: CONNECTED");
             cache.sftp = sftp;
-            print("Connected");
             callback(sftp);
         });
     });
 
-    client.on('error', function (err) {
-        print('sftp', err);
+    client.on('error', function(err) {
+        print('SFTP: ', err);
         reject(err);
     });
 
-    client.on('close', function (hadError) {
+    client.on('close', function(hadError) {
         if (!cache.finished) {
-            print('sftp', "SFTP abrupt closure");
+            print('SFTP: ABRUPT CLOSURE');
         }
-        print('Connection :: close', hadError ? "with error" : "");
+        print('CLIENT :: CLOSED', hadError ? "with error" : "");
     });
 
     // connect
     client.connect({
-        host: options.host,
-        port: options.port || 22,
-        username: options.username,
-        password: options.password,
-        readyTimeout: options.timeout || 2000
+        host: opts.host,
+        port: opts.port || 22,
+        username: opts.username,
+        password: opts.password,
+        readyTimeout: opts.timeout || 2000
     });
-}
+};
 
+/**
+ * [uploadedCache description]
+ * @return {[type]} [description]
+ */
+SftpClient.prototype.uploadedCache = function() {
+    let self = this,
+        opts = self.options,
+        cache = self.cache,
+        cacheFilePath;
+
+    if(!opts.cache) return;
+
+    cache.uploaded = {};
+    cacheFilePath = cache.cacheFilePath = path.join(process.cwd(), 'sftp-uploaded-cache.json');
+
+    try {
+        cache.uploaded = require(cacheFilePath) || {};
+    }catch(e){
+        // console.log(" Cache file:", cacheFilePath, "doesn't exist");
+    }
+}
